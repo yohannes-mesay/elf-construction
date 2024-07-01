f1244c4d7bf395d5e842007b74cd00b2b05f37c5
Yohannes Mesay <johnnymesay@gmail.com>
2024-07-01 10:59:34 +0300

    header icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,20 +10,19 @@ import {
   YouTube,
   YoutubeSearchedFor,
 } from "@mui/icons-material";
-import FacebookIcon from "@mui/icons-material/Facebook";
-import { SvgIcon } from "@mui/material";
-import TelegramIcon from "@mui/icons-material/Telegram";
 
 function Header() {
   return (
-    <div className="flex justify-around flex-wrap m-auto gap-2 p-3 bg-gray-200">
+    <div className="flex justify-around items-center flex-wrap m-auto gap-2 p-2 bg-gray-200">
       <div className="flex">
-        <Place />
-        <p className="ml-1 font-sarif">Addis Ababa, ETHIOPIA</p>
+        <Place fontSize="small" />
+        <p className="ml-1 font-sarif text-sm">
+          Addis Ababa, ETHIOPIA
+        </p>
       </div>
       <div className="flex gap-4 flex-wrap">
         <Facebook fontSize="small" />
-        
+
         <Instagram fontSize="small" />
         <YouTube fontSize="small" />
         <Telegram fontSize="small" />
